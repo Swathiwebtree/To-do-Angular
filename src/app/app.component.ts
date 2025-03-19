13a05d08344fcd4690e705a97bed2eb7f2c2595c
Swathiwebtree <swathi@webtreeonline.com>
2025-03-19 11:47:28 +0530

    Initial commit on Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mytodoapp';
+  tasks:any=[];
+  newtask(content:string) {
+    if(content !=''){
+      this.tasks.push({id:this.tasks.length,title:content})
+      console.log(this.tasks)
+    }
+  }
+
+  removetask(id: number){
+    this.tasks = this.tasks.filter((item:{id:number})=> item.id != id)
+  }
+
+
+  removealltasks(){
+    this.tasks = []
+  }
 }
